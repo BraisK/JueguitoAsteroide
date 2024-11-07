b023f4e94a7641888fe6ec82f2978513f2b60413
BraisK <braiscasares26@gmail.com>
2024-11-07 10:11:53 +0100

    07/11/24

---
+++ src/pages/Ejer1.tsx
@@ -0,0 +1,6 @@
+
+export default function Ejer1() {
+    return <div>
+        Esta es la pagina 1
+    </div>
+}
---
+++ src/pages/Ejer2.tsx
@@ -0,0 +1,8 @@
+
+function Ejer2() {
+    return <div>
+        Esta es la pagina 2
+    </div>
+}
+
+export default Ejer2
